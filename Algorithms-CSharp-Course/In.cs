1e7a877b7502d3f4ac8419b05c19f563cbb5c271
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-26 21:04:58 +0300

    Добавьте файлы проекта.

---
+++ Algorithms-CSharp-Course/In.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_CSharp_Course
+{
+    public class In
+    {
+        public static IEnumerable<int> ReadInts(string filePath)
+        {
+            using (TextReader reader = File.OpenText(filePath))
+            {
+                string lastLine;
+                while ((lastLine = reader.ReadLine()) != null)
+                {
+                    yield return int.Parse(lastLine);
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms-CSharp-Course/Program.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms_CSharp_Course;
+using Algorithms_DataStruct_Lib;
+using System.Diagnostics;
+
+var ints = In.ReadInts("kkk.txt").ToArray();
+
+var watch = new Stopwatch();
+watch.Start();
+
+var triplets = ThreeSum.Count(ints);
+
+watch.Stop();
+
+Console.WriteLine($"Количество триплетов равных нулю: {triplets}");
+Console.WriteLine($"Время выполнения: {watch.Elapsed:g}");
+
+Console.Read();
+
+
+
