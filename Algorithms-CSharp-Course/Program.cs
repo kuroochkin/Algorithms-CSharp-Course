0b4c7beece1ce3e8e200e1d486d617d0ff2dd79e
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-14 18:19:08 +0300

    Работа с Node

--- Algorithms-CSharp-Course/Program.cs
+++ Algorithms-CSharp-Course/Program.cs
@@ -1,7 +1,30 @@
 ﻿using Algorithms_CSharp_Course;
 using Algorithms_DataStruct_Lib;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Node first = new Node() { Value = 5 };
+        Node second = new Node() { Value = 1 };
+        Node third = new Node() { Value = 3 };
 
-ListDemo.Run();
-Console.Read();
+        PrintOutLinkedList(first);
+
+        
+
+        Console.Read();
+
+        
+    }
+    private static void PrintOutLinkedList(Node node)
+    {
+        while (node != null)
+        {
+            Console.WriteLine($"Node = {node.Value}");
+            node = node.Next;
+        }
+    }
+}
---
+++ Algorithms-DataStruct-Lib/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+}
