77a6513fac206bc790dd2f1d945b23aacc574e5e
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-14 17:24:56 +0300

    Наработки в ListDemo

---
+++ Algorithms-CSharp-Course/ListDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Algorithms_CSharp_Course
+{
+    public class ListDemo
+    {
+        public static void Run()
+        {
+            List<int> list = new List<int>();
+            LogCountAndCapacity(list);
+
+            for(int i = 0; i < 16; i++)
+            {
+                list.Add(i);
+                LogCountAndCapacity(list);
+            }
+
+            for(int i = 10; i > 0; i--)
+            {
+                list.RemoveAt(i - 1);
+                LogCountAndCapacity(list);
+            }
+
+            Console.Read();
+        }
+
+        public static void ApiMembers()
+        {
+            var list = new List<int>() { 1, 0, 5, 3, 4 };
+            list.Sort();
+        }
+
+        private static void LogCountAndCapacity(List<int> list)
+        {
+            Console.WriteLine($"Count = {list.Count}. Capacity = {list.Capacity}");
+        }
+    }
+}
--- Algorithms-CSharp-Course/Program.cs
+++ Algorithms-CSharp-Course/Program.cs
@@ -3,27 +3,5 @@ using Algorithms_DataStruct_Lib;
 using System.Diagnostics;
 
 
-int[] arr = new int[] { 4, 3, 8, 1, 5 , -5, -10, 12};
-
-var sort = new Sorting();
-
-
-var watch = new Stopwatch();
-watch.Start();
-
-Sorting.MergeSort(arr);
-
-watch.Stop();
-
-for(int i = 0; i < arr.Length; i++)
-{
-    Console.WriteLine(arr[i] + " ");
-}
-
-
-Console.WriteLine($"Время выполнения: {watch.Elapsed:g}");
-
+ListDemo.Run();
 Console.Read();
-
-
-
